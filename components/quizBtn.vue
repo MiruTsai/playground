e3e8eb8c703e832a163cfea7462fd6fec4a669f7
MiruTsai <zct398@gmail.com>
2023-07-30 21:21:22 +0800

    新增回答按鈕元件

---
+++ components/quizBtn.vue
@@ -0,0 +1,10 @@
+<template>
+    <button class="btn m-2" @click="handler"> {{ text }}</button>
+</template>
+<script setup>
+const props = defineProps(['text'])
+const emit = defineEmits(['updateValue'])
+const handler = () =>{
+    emit('answer', props.text);
+}
+</script>
