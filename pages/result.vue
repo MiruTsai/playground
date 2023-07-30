5977667beb7fd2aaa20493288c2ebfdd961a9c1c
MiruTsai <zct398@gmail.com>
2023-07-30 21:22:15 +0800

    新增 result 頁面

---
+++ pages/result.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex h-screen w-screen items-center justify-center">
+        <div>
+            <h2>答對：<span class="font-bold">{{ right }}</span>題</h2>
+            <h2>答錯：<span class="font-bold">{{ wrong }}</span>題</h2>
+            <button class="btn"><nuxt-link to="./face">再玩一次</nuxt-link></button>
+        </div>
+    </div>
+</template>
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useCountStore } from '../stores/countStore';
+const countStore = useCountStore();
+const { right, wrong } = storeToRefs(countStore);
+</script>
